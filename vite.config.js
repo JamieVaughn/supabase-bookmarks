ae858ee99bc4ac1644acb39b552fc91355e006a3
JamieVaughn <wxv6@case.edu>
2023-03-30 07:00:21 -0400

    try loadEnv

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/supabase-bookmarks",
-  build: {
-    outDir: 'docs'
+export default defineConfig(({ command, mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    base: "/supabase-bookmarks",
+    build: {
+      outDir: 'docs'
+    },
+    define: {
+      __VITE_SITE_URL__: env.VITE_SITE_URL,
+      __VITE_ANON_KEY__: env.VITE_ANON_KEY,
+    },
   }
 })
