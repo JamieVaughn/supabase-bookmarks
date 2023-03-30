a985d5e372aed423baee8a10c673c7d23a0d55ea
JamieVaughn <wxv6@case.edu>
2023-03-30 07:25:17 -0400

    go back to default vit config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode}) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    plugins: [react()],
-    define: {
-      __VITE_SITE_URL__: env.VITE_SITE_URL,
-      __VITE_ANON_KEY__: env.VITE_ANON_KEY,
-    },
-  }
+export default defineConfig({
+  plugins: [react()],
 })
