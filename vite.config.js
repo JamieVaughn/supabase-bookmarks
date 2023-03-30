627de1e2e37c63077c720caa8853c5b1a4df489e
JamieVaughn <wxv6@case.edu>
2023-03-30 07:14:36 -0400

    remove docs and build outdir, baseurl

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,6 @@ export default defineConfig(({ command, mode}) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
     plugins: [react()],
-    base: "/supabase-bookmarks",
-    build: {
-      outDir: 'docs'
-    },
     define: {
       __VITE_SITE_URL__: env.VITE_SITE_URL,
       __VITE_ANON_KEY__: env.VITE_ANON_KEY,
